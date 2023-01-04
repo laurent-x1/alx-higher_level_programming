ee5e3adf0757d1dfd88e4cd655351a39dd116ceb
Olatunbosun <o.oyindasola@outlook.com>
2023-01-04 14:15:03 -0800

    task updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 
 /**
@@ -8,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
